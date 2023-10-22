c5f201ce45dde42f51486c61bb6bfaf35c72d183
Tyler Richard <fluxodyssey@gmail.com>
2023-10-22 16:47:55 -0400

    fixed issue with jumping after pressing enter in keyboard

--- project/Player.gd
+++ project/Player.gd
@@ -69,8 +69,9 @@ func _physics_process(delta):
 		print(line_edit.text)
 		line_edit.clear()
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
