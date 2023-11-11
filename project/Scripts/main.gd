3459a0d13d554c77df428e6a289f5918a163e34c
mkerr6872 <kerr.mlee@gmail.com>
2023-11-10 20:39:52 -0500

    basic interaction

--- project/Scripts/main.gd
+++ project/Scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var pg: ProofGraph
+var interface : XRInterface
 
 func initializeProofGraph():
 	pg = ProofGraph.new()
@@ -12,23 +13,28 @@ func initializeProofGraph():
 func _ready():
 	pg = initializeProofGraph()
 	pg.addNode(Vector3(-8,12,-25))
-	#pg.addNode(Vector3(8,6,-25))
-	#pg.addNode(Vector3(0,-3,-25))
-	get_node("ProofGraph/0").setData("Welcome")
-	#get_node("ProofGraph/1").setData("p→q")
-	#get_node("ProofGraph/2").setData("q")
+	pg.addNode(Vector3(8,6,-25))
+	pg.addNode(Vector3(0,-3,-25))
+	get_node("ProofGraph/0").setData("p")
+	get_node("ProofGraph/1").setData("p→q")
+	get_node("ProofGraph/2").setData("q")
 	
-	#pg.addEdge(get_node("ProofGraph/0"), get_node("ProofGraph/2"))
-	#pg.addEdge(get_node("ProofGraph/1"), get_node("ProofGraph/2"))
+	pg.addEdge(get_node("ProofGraph/0"), get_node("ProofGraph/2"))
+	pg.addEdge(get_node("ProofGraph/1"), get_node("ProofGraph/2"))
 	
-	#get_node("ProofGraph/0").setJustification("assume", "Assume")
-	#get_node("ProofGraph/1").setJustification("assume", "Assume")
-	#get_node("ProofGraph/2").setJustification("ifE", "→E")
+	get_node("ProofGraph/0").setJustification("assume", "Assume")
+	get_node("ProofGraph/1").setJustification("assume", "Assume")
+	get_node("ProofGraph/2").setJustification("ifE", "→E")
 	
-	
-	#Loading standard 3D player character
-	var mainMenu = load("res://Scenes/main_menu.tscn").instantiate()
+	var mainMenu = load("res://Scenes/vr_player.tscn").instantiate()
 	self.add_child(mainMenu)
+	
+	interface = XRServer.find_interface("OpenXR")
+	if interface and interface.is_initialized():
+		get_viewport().use_xr = true
+	
+	
+
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
---
+++ project/Scripts/vr_player.gd
@@ -0,0 +1,217 @@
+extends Node3D
+
+#@onready var line_edit = $"../LineEdit"
+#@onready var virtual_keyboard_2d = $"../CanvasLayer/VirtualKeyboard2D"
+@onready var ray = $"XROrigin3D/Right/rHand/RayCast3D"
+@onready var rightHand = $"XROrigin3D/Right"
+@onready var head = $"../"
+@onready var pointerPG = head.pg
+@onready var keyboard = $"XROrigin3D/Left/Keyboard"
+
+#Movement variables
+var SPEED = 15.0
+var JUMP_VELOCITY = 15.0
+const TURN_SPEED = 0.03
+
+
+#Selection variables
+@export var selectionCount = 0
+@export var selectArray: Array[LogNode]
+const RAY_LENGTH = 1000
+
+#Mode variables
+enum modeTypes {CREATE_NODE, INPUT_DATA, CONNECT, DELETE_EDGE, DELETE_NODE, MOVE_NODE}
+var playerMode = modeTypes.MOVE_NODE
+var selectGate = 2
+
+#Colors
+const regularColor = Color(0.5, 0.75, 0.75, 0.25)
+const selectColor = Color(0.75, 0.75, 0.5, 0.25)
+const newColor = Color(0.25, 0.25, 0.25, 0.75)
+
+#Create Node flags
+var newMeshExists = false
+var newMeshPointer
+var newMeshOffset = 15
+const offsetMax = 50
+const offsetMin = 10
+
+#Move node vars
+var distanceToNode
+var blockPOS
+var offsetPosition
+var changeDistance = 0
+
+#Input variables
+var inputFlag = false
+
+func rayTraceSelect():
+	if selectionCount >= selectGate:
+		return	
+
+	var nodeSelect = ray.get_collider()
+	if nodeSelect != null:
+		if nodeSelect.get_parent().get_class() == "LogNode":
+			var pNode = nodeSelect.get_parent()
+			if selectArray.find(pNode) != -1:
+				selectArray.erase(pNode)
+				selectionCount -= 1
+				pNode.get_child(0).get_material_override().albedo_color = regularColor
+			else:
+				if selectionCount < selectGate-1:
+					selectArray.append(pNode)
+					selectionCount += 1
+					pNode.get_child(0).get_material_override().albedo_color = selectColor
+
+func unselectAll():
+	for pNode in selectArray:
+		pNode.get_child(0).get_material_override().albedo_color = regularColor
+	selectArray.clear()
+	selectionCount = 0
+	inputFlag = false
+	
+func newNodeLocationMesh():
+	var lookDirection = rightHand.get_global_transform().basis.z
+	head = get_node("/root/main")
+	var tempMesh = MeshInstance3D.new()
+	tempMesh.set_name("tempMesh")
+	head.add_child(tempMesh)
+	var box = BoxMesh.new()
+	tempMesh.mesh = box
+	var skin = StandardMaterial3D.new()
+	tempMesh.set_surface_override_material(0,skin)
+	skin.albedo_color = newColor
+	skin.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	tempMesh.global_scale(Vector3(10,5,2))
+	tempMesh.global_position = rightHand.global_position - 10*lookDirection
+	newMeshExists = true
+	newMeshPointer = tempMesh
+
+func clearNewNodeLocationMesh():
+	newMeshPointer.queue_free()
+	newMeshExists = false
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"XROrigin3D/Left/lHand/ModeText".mesh.text = modeTypes.keys()[playerMode]
+	keyboard.hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	match playerMode:
+		modeTypes.CONNECT:
+			pass
+		modeTypes.DELETE_NODE:
+			pass
+		modeTypes.CREATE_NODE:
+			if newMeshExists == false:
+				newNodeLocationMesh()
+			newMeshOffset += changeDistance*.2
+			var lookDirection = rightHand.get_global_transform().basis.z
+			offsetPosition = rightHand.global_position - newMeshOffset*lookDirection
+			newMeshPointer.global_position = offsetPosition
+		modeTypes.MOVE_NODE:
+			var lookDirection = rightHand.get_global_transform().basis.z
+			if selectionCount == 1:
+				distanceToNode+= changeDistance*.2
+				offsetPosition = rightHand.global_position - (distanceToNode)*lookDirection
+				selectArray[0].global_position = offsetPosition
+				pointerPG.updateEdges(selectArray[0])
+		modeTypes.INPUT_DATA:
+			if inputFlag == true:
+				keyboard.show()
+			else:
+				keyboard.hide()
+
+func _on_right_button_pressed(rname):
+	if rname == "trigger_click" and inputFlag == false:
+		rayTraceSelect()
+		match playerMode:
+			modeTypes.CONNECT:
+				pass
+			modeTypes.DELETE_NODE:
+				pass
+			modeTypes.CREATE_NODE:
+				pass
+			modeTypes.MOVE_NODE:
+				if selectionCount == 1:
+					blockPOS = selectArray[0].global_position
+					distanceToNode = (blockPOS - rightHand.global_position).length()
+			modeTypes.INPUT_DATA:
+				if selectionCount == 1:
+					inputFlag = true
+			modeTypes.DELETE_EDGE:
+				pass
+		
+	if rname == "by_button":
+		unselectAll()
+		
+	if rname == "ax_button":
+		match playerMode:
+			modeTypes.CONNECT:
+				if selectionCount == 2:
+					pointerPG.addEdge(selectArray[0], selectArray[1])
+					unselectAll()
+			modeTypes.DELETE_NODE:
+				if selectionCount == 1:
+					pointerPG.removeNode(selectArray[0])
+					unselectAll()
+			modeTypes.CREATE_NODE:
+				clearNewNodeLocationMesh()
+				head = get_node("/root/main")
+				pointerPG = head.pg
+				pointerPG.addNode(offsetPosition)
+			modeTypes.MOVE_NODE:
+				unselectAll()
+			modeTypes.INPUT_DATA:
+				pass
+			modeTypes.DELETE_EDGE:
+				if selectionCount == 2:
+					if selectArray[1].isChild(selectArray[0]):
+						pointerPG.removeEdge(selectArray[1], selectArray[0])
+					else:
+						pointerPG.removeEdge(selectArray[0], selectArray[1])
+					unselectAll()
+		
+
+func _on_left_button_pressed(lname):
+	if lname == "trigger_click" and inputFlag == false:
+		inputFlag = false
+		if playerMode < 5:
+			playerMode = playerMode+1
+		else:
+			playerMode = modeTypes.CREATE_NODE
+			
+		match playerMode:
+			modeTypes.CREATE_NODE:
+				selectGate = 0
+			modeTypes.INPUT_DATA:
+				selectGate = 2
+				clearNewNodeLocationMesh()
+			modeTypes.CONNECT: 
+				selectGate = 3
+			modeTypes.DELETE_EDGE:
+				selectGate = 3
+			modeTypes.DELETE_NODE:
+				selectGate = 2
+			modeTypes.MOVE_NODE:
+				selectGate = 2
+		$"XROrigin3D/Left/lHand/ModeText".mesh.text = modeTypes.keys()[playerMode]
+		unselectAll()
+	
+func _on_right_input_vector_2_changed(rjname, value):
+	if rjname == "primary" and inputFlag == false:
+		match playerMode:
+			modeTypes.CONNECT:
+				pass
+			modeTypes.DELETE_NODE:
+				pass
+			modeTypes.CREATE_NODE:
+				changeDistance = value[1]
+			modeTypes.MOVE_NODE:
+				if selectionCount == 1:
+					changeDistance = value[1]
+			modeTypes.INPUT_DATA:
+				pass
+		
