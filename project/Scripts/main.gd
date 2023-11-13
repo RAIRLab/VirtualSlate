c2c898c3349617d412e18497f6f0d0e894b366f8
mkerr6872 <kerr.mlee@gmail.com>
2023-11-13 15:04:52 -0500

    keyboard tweaks

--- project/Scripts/main.gd
+++ project/Scripts/main.gd
@@ -9,31 +9,33 @@ func initializeProofGraph():
 	self.add_child(pg)
 	return pg
 	
+func demo():
+	pg.addNode(Vector3(-2,3,-6))
+	pg.addNode(Vector3(2,1.5,-6))
+	pg.addNode(Vector3(0,-1.5,-6))
+	
+	get_node("ProofGraph/0").setData("p")
+	get_node("ProofGraph/1").setData("p→q")
+	get_node("ProofGraph/2").setData("q")
+	
+	pg.addEdge(get_node("ProofGraph/0"), get_node("ProofGraph/2"))
+	pg.addEdge(get_node("ProofGraph/1"), get_node("ProofGraph/2"))
+	
+	get_node("ProofGraph/0").setJustification("assume", "Assume")
+	get_node("ProofGraph/1").setJustification("assume", "Assume")
+	get_node("ProofGraph/2").setJustification("ifE", "→E")
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pg = initializeProofGraph()
+	
 	interface = XRServer.find_interface("OpenXR")
 	if interface and interface.is_initialized():
 		get_viewport().use_xr = true
 	
-	pg = initializeProofGraph()
-	#pg.addNode(Vector3(-2,3,-6))
-	#pg.addNode(Vector3(2,1.5,-6))
-	#pg.addNode(Vector3(0,-1.5,-6))
-	#get_node("ProofGraph/0").setData("p")
-	#get_node("ProofGraph/1").setData("p→q")
-	#get_node("ProofGraph/2").setData("q")
-	
-	#pg.addEdge(get_node("ProofGraph/0"), get_node("ProofGraph/2"))
-	#pg.addEdge(get_node("ProofGraph/1"), get_node("ProofGraph/2"))
-	
-	#get_node("ProofGraph/0").setJustification("assume", "Assume")
-	#get_node("ProofGraph/1").setJustification("assume", "Assume")
-	#get_node("ProofGraph/2").setJustification("ifE", "→E")
-	
 	var mainMenu = load("res://Scenes/vr_player.tscn").instantiate()
 	self.add_child(mainMenu)
 	
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	pass
